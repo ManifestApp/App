503cdac9bda8a47f9ce68e8c81e768fae1514a90
Laurent Meyer <laurent.m.meyer@gmail.com>
2019-01-27 12:06:18 +0100

    [WIP] Date Picker is on the way

--- app/utils/languages.js
+++ app/utils/languages.js
@@ -1,9 +1,12 @@
 import RNLanguages from 'react-native-languages';
 import i18n from 'i18n-js';
-
+import moment from "moment";
 import fr from '../translations/fr.json';
+import LocaleCode from "locale-code"
 
-i18n.locale = RNLanguages.language;
+const selected_locale = LocaleCode.getLanguageCode(RNLanguages.language);
+i18n.locale = selected_locale;
+moment().locale(selected_locale);
 i18n.fallbacks = true;
 i18n.defaultLocale = 'fr';
 i18n.translations = { fr };
